8eeaf3f8bbe75a373c23571df21db527a888b9af
Christian Mader <Cmader555@users.noreply.github.com>
2019-05-22 14:16:37 -0700

    survey response parsed

--- app/data/friend.js
+++ app/data/friend.js
@@ -17,90 +17,90 @@ let friends = [
       2,
       1
     ]
-  }, 
+  },
 
   {
-    name:"Lego Rob",
-    photo:"https://randomuser.me/api/portraits/lego/7.jpg",
-    scores:[
-        5,
-        4,
-        2,
-        4,
-        1,
-        2,
-        3,
-        5,
-        3,
-        1
-      ]
-  }, 
+    name: "Lego Rob",
+    photo: "https://randomuser.me/api/portraits/lego/7.jpg",
+    scores: [
+      5,
+      4,
+      2,
+      4,
+      1,
+      2,
+      3,
+      5,
+      3,
+      1
+    ]
+  },
 
   {
-    name:"Emma",
-    photo:"https://randomuser.me/api/portraits/women/81.jpg",
-    scores:[
-        3,
-        2,
-        1,
-        5,
-        4,
-        3,
-        1,
-        2,
-        5,
-        1
-      ]
-  }, 
+    name: "Emma",
+    photo: "https://randomuser.me/api/portraits/women/81.jpg",
+    scores: [
+      3,
+      2,
+      1,
+      5,
+      4,
+      3,
+      1,
+      2,
+      5,
+      1
+    ]
+  },
 
   {
-    name:"Dave",
-    photo:"https://randomuser.me/api/portraits/men/74.jpg",
-    scores:[
-        2,
-        3,
-        4,
-        2,
-        2,
-        3,
-        3,
-        5,
-        5,
-        5
-      ]
-  }, 
+    name: "Dave",
+    photo: "https://randomuser.me/api/portraits/men/74.jpg",
+    scores: [
+      2,
+      3,
+      4,
+      2,
+      2,
+      3,
+      3,
+      5,
+      5,
+      5
+    ]
+  },
 
   {
-    name:"Jenny",
-    photo:"https://randomuser.me/api/portraits/women/85.jpg",
-    scores:[
-        2,
-        2,
-        1,
-        5,
-        2,
-        3,
-        4,
-        1,
-        1,
-        1
-      ]
-  }, 
+    name: "Jenny",
+    photo: "https://randomuser.me/api/portraits/women/85.jpg",
+    scores: [
+      2,
+      2,
+      1,
+      5,
+      2,
+      3,
+      4,
+      1,
+      1,
+      1
+    ]
+  },
   {
-    name:"Ahmed",
-    photo:"https://randomuser.me/api/portraits/men/37.jpg",
-    scores:[
-        1,
-        3,
-        4,
-        4,
-        4,
-        2,
-        5,
-        5,
-        4,
-        5
-      ]
+    name: "Ahmed",
+    photo: "https://randomuser.me/api/portraits/men/37.jpg",
+    scores: [
+      1,
+      3,
+      4,
+      4,
+      4,
+      2,
+      5,
+      5,
+      4,
+      5
+    ]
   }
 ];
 
