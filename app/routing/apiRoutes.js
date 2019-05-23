18d038f122780e187b9af558b4611cbe343dd42a
Christian Mader <Cmader555@users.noreply.github.com>
2019-05-23 13:39:15 -0700

    fixed match problem, fixing adding new user error

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,48 +5,59 @@ let router = express.Router();
 
 
 
-
+//Actual API, displays all friends objects in JSON format
 router.get("/friends", function (req, res) {
 
     return res.json(friends);
 });
 
-
+// posting new person to friends API/comparing "friends" for closest match
 router.post("/friends", function (req, res) {
 
-    let newFriends = req.body;
 
-    let user = parseInt(newFriends.scores); 
-
-    let matchName; 
-    let matchPhoto; 
-
-    //value to be changed later
-    
-    
-    for (let  i = 0; i < friends.length; i++ ) {
-
-        let difference = 0; 
-        let totalDifference =100; 
-        for (let j = 0; j <user.length; j++) {
-
-            difference += Math.abs(friends[i].scores[j] - user[j])
+    let newFriends = req.body.scores;
 
 
+
+    //let user = parseInt(newFriends.scores);
+
+
+    let matchName;
+    let matchPhoto;
+
+
+    let totalDifference = 100;
+   
+
+
+    for (let i = 0; i < friends.length; i++) {
+
+
+       
+        let difference = 0;
+
+        for (let j = 0; j < friends[i].scores.length; j++) {
+
+
+            difference += Math.abs(friends[i].scores[j] - parseInt(req.body.scores[j])); 
+            console.log("j loop diff" + difference)
         }
+
 
         if (difference < totalDifference) {
 
-            totalDifference = difference; 
-            matchName = friends[i].name; 
-            matchPhoto = friends[i].photo; 
+            totalDifference = difference;
+            console.log("total" + totalDifference)
+            matchName = friends[i].name;
+            matchPhoto = friends[i].photo;
         }
 
 
     }
 
+
     friends.push(newFriends);
-    res.json({matchName:matchName, matchPhoto:matchPhoto});
+    res.json({ matchName: matchName, matchPhoto: matchPhoto });
 
 });
 
