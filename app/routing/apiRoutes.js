61ea1e4ad52ff423d253efc3dc7959506ba58259
Christian Mader <Cmader555@users.noreply.github.com>
2019-05-22 21:48:07 -0700

    all routes working, need to compare arrays

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,10 +7,60 @@ let router = express.Router();
 
 
 router.get("/friends", function (req, res) {
-    
+
     return res.json(friends);
 });
 
 
+router.post("/friends", function (req, res) {
 
-module.exports = router; 
+    let newFriends = req.body;
+
+    friends.push(newFriends);
+    //res.json(true);
+
+
+});
+
+
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// module.exports = function (app) {
+
+//     app.get("/api/friends", function (req, res) {
+
+//          res.json(friends);
+//     });
+
+//     app.post("/api/friends", function (req, res) {
+
+//         let newFriends = req.body; 
+
+//         friends.push(newFriends);
+//         //res.json(true);
+
+
+//     });
+
+
+
+// }; 
--- server.js
+++ server.js
@@ -13,8 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/", router)
-// app.use("/survey", router)
 app.use("/api", routerAPI);
+// require("./app/routing/htmlroutes")(app);
+// require("./app/routing/apiroutes.js")(app);
 
 
 app.listen(PORT, function(){
